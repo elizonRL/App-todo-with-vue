4c0d860223e7daad777acdcf5efa135f709fa8da
elizonRL <elizorodriguez16@gmail.com>
2023-03-31 10:30:25 -0400

    Aplicacion Con vue curso

---
+++ src/App.vue
@@ -0,0 +1,109 @@
+<template>
+  <nav class="navbar">
+    <img src="./assets/logo.svg" alt="logo vue" width="50" />
+    <div class="brand">Todo list app</div>
+  </nav>
+  <main class="container">
+    <Alert
+      message="Todo Title is required"
+      :show="showAlert"
+      @close="showAlert = flase"
+      type="danger"
+    />
+    <section>
+      <form class="add-todo-form">
+        <input v-model="todoTitle" type="text" placeholder="Todo Title" />
+        <div>
+          <button @click.prevent="addTodo">Add Todo</button>
+        </div>
+      </form>
+    </section>
+    <section>
+      <div class="todo" v-for="todo in todos" :key="todo.id">
+        <p>{{ todo.title }}</p>
+        <div>
+          <button @click="remuveTodo(todo)" class="remuve-todo-btn">
+            &times;
+          </button>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+<script>
+import Alert from "./components/alert.vue";
+
+export default {
+  components: {
+    Alert,
+  },
+  data() {
+    return {
+      todoTitle: "",
+      todos: [],
+      showAlert: false,
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.todoTitle === "") {
+        this.showAlert = true;
+        return;
+      }
+      this.todos.push({
+        title: this.todoTitle,
+        id: Math.floor(Math.random() * 1000),
+      });
+    },
+    remuveTodo(todoTitle) {
+      this.todos = this.todos.filter((todo) => todo !== todoTitle);
+    },
+  },
+  components: { Alert },
+};
+</script>
+
+<style scoped>
+.navbar {
+  display: flex;
+  background-color: var(--navbar-color);
+  padding: 20px;
+  margin-bottom: 30px;
+}
+.brand {
+  font-size: 2rem;
+}
+.add-todo-form {
+  display: flex;
+  justify-content: space-between;
+}
+.add-todo-form input {
+  width: 80%;
+  border: solid 2px var(--accent-color);
+}
+.add-todo-form button {
+  background-color: var(--accent-color);
+  color: var(--text-color);
+  border: none;
+  height: 50px;
+}
+.todo {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: var(--accent-color);
+  margin-top: 30px;
+  padding: 0 20px 0 20px;
+  border-radius: 10px;
+}
+.remuve-todo-btn {
+  border-radius: 50%;
+  border: none;
+  height: 40px;
+  width: 40px;
+  font-size: 30px;
+  color: var(--text-color);
+  background-color: var(--danger-color);
+  cursor: pointer;
+}
+</style>

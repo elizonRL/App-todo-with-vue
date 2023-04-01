1d2e28a6d8d509969edb7ca5936158b3eef7f220
elizonRL <elizorodriguez16@gmail.com>
2023-04-01 10:14:19 -0400

    errores de codigo

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,46 +1,46 @@
 <template>
   <div v-if="show" class="alert" :style="{ backgroundColor }">
-    <div>{{message}}</div>
+    <div>{{ message }}</div>
     <div @click="$emit('close')" class="close-alert">&times;</div>
   </div>
 </template>
 
 <script>
-export default{
-    props: {
-        message: {
-          required: true,
-          type: String,
-        },
-        show:{
-            required: true,
-            type: Boolean,
-        },
-        type:{
-            default: "danger",
-            validator(value){
-                return["danger","warning", "info"].includes(value)
-            }
-        }
+export default {
+  props: {
+    message: {
+      required: true,
+      type: String,
     },
-    computed:{
-        backgroundColor(){
-            const options ={
-                danger: "var(--danger-color)",
-                info: "var(--info-color)",
-                warning: "var(--warning-color)",
-            }
-            return options[this.type];
-        }
+    show: {
+      required: true,
+      type: Boolean,
     },
+    type: {
+      default: "danger",
+      validator(value) {
+        return ["danger", "warning", "info"].includes(value);
+      },
+    },
+  },
+  computed: {
+    backgroundColor() {
+      const options = {
+        danger: "var(--danger-color)",
+        info: "var(--info-color)",
+        warning: "var(--warning-color)",
+      };
+      return options[this.type];
+    },
+  },
 
-    emits: ['close'],
+  emits: ["close"],
 
-    methods: {
-        closeAlert(){
-            this.$emit('close');
-        }
-    }
+  methods: {
+    closeAlert() {
+      this.$emit("close");
+    },
+  },
 };
 </script>
 
@@ -54,8 +54,8 @@ export default{
   border-radius: 10px;
   height: 50px;
 }
-.close-alert{
-    font-size: 50px;
-    cursor: pointer;
+.close-alert {
+  font-size: 50px;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/Btn.vue
@@ -0,0 +1,43 @@
+<template>
+  <button :style="{ backgroundColor }" @click.prevent="$emit('click')" class="{circle: applayCircleClass}">
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  props: {
+    type: {
+      default: "success",
+      validator(value) {
+        return ["danger", "warning", "info", "success"].includes(value);
+      },
+    },
+  },
+  computed: {
+    backgroundColor() {
+      const options = {
+        danger: "var(--danger-color)",
+        info: "var(--info-color)",
+        warning: "var(--warning-color)",
+        success: "var(--accent-color)",
+      };
+      return options[this.type];
+    },
+    applayCircleClass() {
+      return this.circle;
+    },
+  },
+  emit: ["click"],
+};
+</script>
+
+<style scoped>
+button {
+  color: var(--text-color);
+  border: none;
+  cursor: pointer;
+}
+.circle {
+  border-radius: 50%;
+}
+</style>
